fcf8d2e92b7ce5a2039c739f870588192cb2c5c5
MarionFoto <marionacp@gmail.com>
2017-07-12 22:33:27 -0400

    ahora funciona

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 $(document).ready(function($){
-	$.ajax({
-		url:'http://pokeapi.co/api/v2/pokemon',
-		type:'GET',
-		datatype:'json',
-		data:{
-			limit:'719',
-		},
-	})
-})
+        $.ajax({
+            url: 'http://pokeapi.co/api/v2/pokemon/',
+            type: 'GET',
+            datatype: 'json',
+            data: {
+                limit : '719'
+            },
+        })
+        .done(function(response) {
+            for (var i = 1; i < 719; i++) {
+            var pokePic = $("<a class='waves-effect waves-light btn' href='#modal1'><div class = 'col l3 m3 s3 xs1'><img class = 'pokepoke' src=http://pokeapi.co/media/img/" + i + ".png id=" + i + "/>");
+            $('.pokemon').append(pokePic);
+            }
+            $('#modal1').modal('open');
+            $('#modal1').modal('close');
+        })
+        .fail(function(){
+            console.log('error');
+        })
+        .always(function(){
+            console.log('complete');
+        });
+    });
